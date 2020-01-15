312d2255cde5c01262630ec2e46e69a751c313c9
Sonali Oswal <sonali.oswal@agrostar.in>
2020-01-15 15:42:21 +0530

    Updates

---
+++ precache-manifest.b7904e9f769aee92baecabe2ba963d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d748b80204718e55b5eae10b911801ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a0edee608369864bb5",
+    "url": "/static/css/main.0c04ad2d.chunk.css"
+  },
+  {
+    "revision": "a74a65c7b2f9aaf42928",
+    "url": "/static/js/2.10ac5fab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.10ac5fab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91a0edee608369864bb5",
+    "url": "/static/js/main.ffdddef5.chunk.js"
+  },
+  {
+    "revision": "6e2b69a263b29601e3ec",
+    "url": "/static/js/runtime-main.f7fd6fdd.js"
+  }
+]);
