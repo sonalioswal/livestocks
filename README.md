04671369efefe2e23a2e614bcdf3efae8550c91c
Sonali Oswal <sonali.oswal@agrostar.in>
2020-01-16 15:08:58 +0530

    assignment is in progress

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-hello
+<!-- github page url -->
+
+https://sonalioswal.github.io/livestocks
+
+<!-- Steps to run the app manualy -->
+
+1. yarn
+2. yarn build
+3. yarn start
--- src/component/stockList/stockList.js
+++ src/component/stockList/stockList.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
-import StockItem from "../stockItem/stockItem";
+import moment from "moment";
+// styles
 import "./stockList.scss";
 import "../liveStockUpdates/liveStockUpdates.scss";
 
 class StockList extends Component {
+  getStockValue = stock => {
+    if (stock.currentValue < stock.history.slice(-2)[0].value) {
+      return "price-decreased";
+    } else if (stock.currentValue > stock.history.slice(-2)[0].value) {
+      return "price-increased";
+    } else {
+      return null;
+    }
+  };
+
   getStockList = stocks => {
     return Object.keys(stocks).map((stockName, index) => {
       let currentStock = stocks[stockName];
       return (
-        <StockItem key={index} stockName={stockName} stockData={currentStock} />
+        <tr id={stockName}>
+          <td>{stockName}</td>
+          <td className={this.getStockValue(currentStock)}>
+            {currentStock.currentValue.toFixed(3)}
+          </td>
+          <td>
+            {moment(currentStock.history.slice(-1)[0].time)
+              .startOf()
+              .fromNow()}
+          </td>
+        </tr>
       );
     });
   };
@@ -19,18 +40,16 @@ class StockList extends Component {
         <div className="panel-header">
           <h4>Live Stocks</h4>
         </div>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Value</th>
-                <th>Last Updated</th>
-              </tr>
-            </thead>
-            <tbody>{this.getStockList(this.props.stocks)}</tbody>
-          </table>
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Value</th>
+              <th>Last Updated</th>
+            </tr>
+          </thead>
+          <tbody>{this.getStockList(this.props.stocks)}</tbody>
+        </table>
       </div>
     );
   }
